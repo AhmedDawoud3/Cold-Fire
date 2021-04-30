fb4653e56afcec103407b535e04cb44c2a60db50
Ahmed Dawoud <adawoud1000@hotmail.com>
2021-04-30 20:42:00 +0200

    Random Enemy Colors

--- eGenerator.lua
+++ eGenerator.lua
@@ -1,8 +1,11 @@
 EGen = Class {}
 require 'enemy'
 enemies = {}
-function EGen:NewEnemy(x, y, size)
-    enemy = Enemy(x, y, size)
+function EGen:NewEnemy(x, y, size, r, g, b)
+    r = r or 1
+    g = g or 0.3
+    b = b or 0.26
+    enemy = Enemy(x, y, size, r, g, b)
     table.insert(enemies, enemy)
 end
 
@@ -12,7 +15,10 @@ function EGen:GenerateRandom(numberOfEnemies)
         eSize = math.random(10, 25)
         eX = math.random(20, WIDTH - eSize - 10)
         eY = math.random(20, HEIGHT - 20)
-        EGen:NewEnemy(eX, eY, eSize)
+        eR = math.random(9000, 10000) / 10000
+        eG = math.random(2000, 4000) / 10000
+        eB = math.random(2000, 4000) / 10000
+        EGen:NewEnemy(eX, eY, eSize, eR, eG, eB)
     end
 end
 
--- enemy.lua
+++ enemy.lua
@@ -1,11 +1,11 @@
 Enemy = Class {}
 
-function Enemy:init(x, y, size)
-    self.x, self.y, self.size = x, y, size
+function Enemy:init(x, y, size, r, g, b)
+    self.x, self.y, self.size, self.r, self.g, self.b = x, y, size, r, g, b
 end
 
 function Enemy:Render()
-    love.graphics.setColor(1, 0.3, 0.26, 1)
+    love.graphics.setColor(self.r, self.g, self.b)
     love.graphics.circle("fill", self.x, self.y, self.size)
     love.graphics.setColor(1, 1, 1, 1)
 end

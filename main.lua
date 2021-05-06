0fc50b8e83cf9402519fbfcc9524900e150c4dda
Ahmed Dawoud <adawoud1000@hotmail.com>
2021-05-06 12:28:43 +0200

    Fix Generator Bug

--- main.lua
+++ main.lua
@@ -1,20 +1,20 @@
 Class = require 'Utils/class'
+Fonts = {
+    ["main"] = love.graphics.newFont('Fonts/font0.ttf', 50),
+    ["Big"] = love.graphics.newFont('Fonts/font0.ttf', 100),
+    ["Secondary"] = love.graphics.newFont('Fonts/Font1.ttf', 40)
+}
 require 'Player/TouchContralls'
 require 'Player/character'
 require 'Enemy/eGenerator'
 require 'GameManager/GameManager'
-WIDTH = love.graphics.getWidth()
-HEIGHT = love.graphics.getHeight()
+WIDTH = 391
+HEIGHT = 862
 
 function love.load()
     math.randomseed(os.time())
     love.window.setMode(391, 862)
     love.window.setTitle("Cold Fire")
-    Fonts = {
-        ["main"] = love.graphics.newFont('Fonts/font0.ttf', 32),
-        ["Big"] = love.graphics.newFont('Fonts/font0.ttf', 100),
-        ["Secondary"] = love.graphics.newFont('Fonts/Font1.ttf', 40)
-    }
     GameManager = GameManager()
 end
 
